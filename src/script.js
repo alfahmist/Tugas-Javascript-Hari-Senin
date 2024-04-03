448ed11d6f94ff8e67872e860ac2a77f1080ac59
alfahmist <gst.alfahmi@gmail.com>
2024-04-03 20:26:06 +0700

    display item

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 // Get All Products
-let container = document.getElementById("container");
-let categorySection = document.getElementById("categorySection");
-let card = document.getElementById("card");
+const container = document.getElementById("container");
+const categorySection = document.getElementById("categorySection");
+
+let defaultId = 1;
+let lastId = defaultId;
 
 async function getProducts() {
 	const response = await fetch(
@@ -13,8 +15,8 @@ async function getProducts() {
 
 async function getCategory() {
 	const response = await fetch("https://api.escuelajs.co/api/v1/categories");
-	const category = await response.json();
-	return category;
+	const categories = await response.json();
+	return categories;
 }
 
 async function getCategoryList() {
@@ -24,56 +26,138 @@ async function getCategoryList() {
 	});
 }
 
-async function displayCategoryList() {
-	let categories = await getCategory();
-	let categoryList = categories.map(({ name }) => {
-		return name;
-	});
-
-	let categoryListSliced = categoryList.slice(0, 4);
-	categoryListSliced.forEach((category) => {
-		let a = createDiv(
-			"a",
-			"border-b-2",
-			"border-transparent",
-			"hover:border-black",
-			"cursor-pointer"
-		);
-		a.innerHTML = category;
-		categorySection.append(a);
-	});
+async function getProductsById(id) {
+	const response = await fetch(
+		`https://api.escuelajs.co/api/v1/products/?categoryId=${id}&offset=0&limit=5`
+	);
+	const productsById = await response.json();
+	return productsById;
 }
 
-async function displayProducts() {
-	const products = await getProducts();
+async function displayProductsById(id, clicked) {
+	let cards = document.getElementsByClassName("card");
+
+	const products = await getProductsById(id);
+
+	// console.log(products);
 	products.forEach((product) => {
 		// console.log(product);
-		let { id, title, price, description, images } = product;
-		let a = createDiv(
+		const {
+			title,
+			price,
+			description,
+			images,
+			category: { name },
+		} = product;
+		// console.log(name);
+		const a = createDiv(
 			"a",
+			"card",
 			"p-5",
 			"w-60",
 			"border-2",
 			"rounded",
 			"cursor-pointer"
 		);
-		let img = createDiv("img", "mb-4");
-		let h1 = createDiv("h1", "mb-4");
-		let p = createDiv("p", "font-bold");
+		const img = createDiv("img", "mb-4");
+		const pCategory = createDiv("p", "text-xs", "mb-2", "font-semibold");
+		const h1 = createDiv("h1", "mb-4");
+		const p = createDiv("p", "font-bold");
 
 		// a.href = id;
 		img.src = "headphone.jpeg";
+		pCategory.innerHTML = `${name}`;
 		h1.innerHTML = title;
 		p.innerHTML = `$ ${price}`;
+
 		container.append(a);
 		a.append(img);
+		a.append(pCategory);
 		a.append(h1);
 		a.append(p);
 	});
+
+	// if (lastId == id && clicked == true) {
+	// 	let cards = document.getElementsByClassName("card");
+	// 	// console.log(as);
+	// 	for (item of cards) {
+	// 		item.remove();
+	// 	}
+	// 	lastId == id;
+	// } else if (lastId != id && clicked == true) {
+	// 	let cards = document.getElementsByClassName("card");
+	// 	// console.log(as);
+	// 	for (item of cards) {
+	// 		item.remove();
+	// 	}
+	// 	lastId == id;
+	// }
 }
 
-displayProducts();
+async function displayCategoryList() {
+	const categories = await getCategory();
+	const categoryList = categories.map(({ id, name }) => {
+		return { id, name };
+	});
+
+	const categoryListSliced = categoryList.slice(0, 4);
+	categoryListSliced.forEach((category) => {
+		const { id, name } = category;
+		const a = createDiv(
+			"a",
+			"border-b-2",
+			"border-transparent",
+			"hover:border-black",
+			"cursor-pointer"
+		);
+		a.innerHTML = name;
+		a.setAttribute(`onClick`, `displayProductsById(${id + "," + true})`);
+		categorySection.append(a);
+	});
+}
+
+// async function displayProducts() {
+// 	const products = await getProducts();
+// 	products.forEach((product) => {
+// 		console.log(product);
+// 		const {
+// 			id,
+// 			title,
+// 			price,
+// 			description,
+// 			images,
+// 			category: { name },
+// 		} = product;
+// 		// console.log(name);
+// 		const a = createDiv(
+// 			"a",
+// 			"p-5",
+// 			"w-60",
+// 			"border-2",
+// 			"rounded",
+// 			"cursor-pointer"
+// 		);
+// 		const img = createDiv("img", "mb-4");
+// 		const h1 = createDiv("h1", "mb-2");
+// 		const pCategory = createDiv("p");
+// 		const p = createDiv("p", "font-bold");
+
+// 		// a.href = id;
+// 		img.src = "headphone.jpeg";
+// 		h1.innerHTML = title;
+// 		p.innerHTML = `$ ${price}`;
+// 		pCategory.innerHTML = `${name}`;
+// 		container.append(a);
+// 		a.append(img);
+// 		a.append(pCategory);
+// 		a.append(h1);
+// 		a.append(p);
+// 	});
+// }
+
+// displayProducts();
 displayCategoryList();
+displayProductsById(defaultId);
 
 function createDiv(tag, ...classes) {
 	let div = document.createElement(tag);
