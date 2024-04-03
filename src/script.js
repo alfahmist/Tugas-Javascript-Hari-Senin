f68082cd38d9ebc8b9e30f374d0b7c4106228a83
alfahmist <gst.alfahmi@gmail.com>
2024-04-03 12:16:38 +0700

    add product to html

--- src/script.js
+++ src/script.js
@@ -1,35 +1,49 @@
 // Get All Products
-
-async function getProducts() {
-    const response = await fetch(
-        "https://api.escuelajs.co/api/v1/products?offset=0&limit=10"
-    );
-    const products = await response.json();
-    return products;
-}
-
 let container = document.getElementById("container");
 let card = document.getElementById("card");
 
-function createDiv(tag, ...classes) {
-    let div = document.createElement(tag);
-    div.classList.add(...classes);
-    return div;
+async function getProducts() {
+	const response = await fetch(
+		"https://api.escuelajs.co/api/v1/products?offset=0&limit=5"
+	);
+	const products = await response.json();
+	return products;
 }
 
-getProducts().then((products) => {
-    // console.log(products);
-    products.forEach((product) => {
-        console.log(product);
-        let { id, title, price, description, images } = product;
-        // let div = document.createElement("div");
-        // div.classList.add("p-5", "bg-red-500");
-        let div = createDiv("div", "p-5", "bg-red-500");
-        let img = createDiv("img");
-        img.src = images[0];
-        img.referrerpolicy = "no-referrer";
-        div.innerHTML = title;
-        container.append(img);
-        container.append(div);
-    });
-});
+async function displayProducts() {
+	const products = await getProducts();
+	products.forEach((product) => {
+		console.log(product);
+		let { id, title, price, description, images } = product;
+		let a = createDiv(
+			"a",
+			"p-5",
+			"w-60",
+			"border-2",
+			"rounded",
+			"cursor-pointer"
+		);
+		let img = createDiv("img", "mb-4");
+		let h1 = createDiv("h1", "mb-4");
+		let p = createDiv("p", "font-bold");
+
+		// a.href = id;
+		img.src = "headphone.jpeg";
+		h1.innerHTML = title;
+		p.innerHTML = `$ ${price}`;
+		container.append(a);
+		a.append(img);
+		a.append(h1);
+		a.append(p);
+	});
+}
+
+displayProducts();
+
+function createDiv(tag, ...classes) {
+	let div = document.createElement(tag);
+	if (classes.length != 0) {
+		div.classList.add(...classes);
+	}
+	return div;
+}
