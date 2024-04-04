2abf0eea38365837e3704acdab54e6a82235f7a2
alfahmist <gst.alfahmi@gmail.com>
2024-04-04 16:44:59 +0700

    menampilkan category yang ada datanya

--- src/script.js
+++ src/script.js
@@ -16,6 +16,8 @@ async function getProducts() {
 async function getCategory() {
 	const response = await fetch("https://api.escuelajs.co/api/v1/categories");
 	const categories = await response.json();
+	// console.log(categories);
+
 	return categories;
 }
 
@@ -26,15 +28,16 @@ async function getCategoryList() {
 	});
 }
 
-async function getProductsById(id) {
+async function getProductsByCategoryId(id) {
 	const response = await fetch(
-		`https://api.escuelajs.co/api/v1/products/?categoryId=${id}&offset=0&limit=5`
+		`https://api.escuelajs.co/api/v1/products/?categoryId=${id}`
+		// `https://api.escuelajs.co/api/v1/products/?categoryId=${id}&offset=0&limit=5`
 	);
 	const productsById = await response.json();
 	return productsById;
 }
 
-async function displayProductsById(id, clicked) {
+async function displayProductsByCategoryId(id, clicked) {
 	if (clicked == true) {
 		container.firstChild.remove();
 	}
@@ -48,7 +51,7 @@ async function displayProductsById(id, clicked) {
 	);
 
 	container.append(containerProduct);
-	const products = await getProductsById(id);
+	const products = await getProductsByCategoryId(id);
 
 	// console.log(products);
 	products.forEach((product) => {
@@ -89,14 +92,26 @@ async function displayProductsById(id, clicked) {
 	});
 }
 
+async function sorting(categories) {
+	console.log(categories);
+	let arr = [];
+	for (const file of categories) {
+		const contents = await getProductsByCategoryId(file.id);
+		if (contents.length != 0) {
+			arr.push(file);
+		}
+	}
+	return arr;
+}
 async function displayCategoryList() {
 	const categories = await getCategory();
 	const categoryList = categories.map(({ id, name }) => {
 		return { id, name };
 	});
 
-	const categoryListSliced = categoryList.slice(0, 4);
-	categoryListSliced.forEach((category) => {
+	let categoryNotNull = await sorting(categoryList);
+	console.log(categoryNotNull);
+	categoryNotNull.forEach((category) => {
 		const { id, name } = category;
 		const a = createDiv(
 			"a",
@@ -106,13 +121,16 @@ async function displayCategoryList() {
 			"cursor-pointer"
 		);
 		a.innerHTML = name;
-		a.setAttribute(`onClick`, `displayProductsById(${id + "," + true})`);
+		a.setAttribute(
+			`onClick`,
+			`displayProductsByCategoryId(${id + "," + true})`
+		);
 		categorySection.append(a);
 	});
 }
 
 displayCategoryList();
-displayProductsById(defaultId);
+displayProductsByCategoryId(defaultId);
 
 function createDiv(tag, ...classes) {
 	let div = document.createElement(tag);
